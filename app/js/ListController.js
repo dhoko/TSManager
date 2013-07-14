234470c1d48e9ccaf3aaed2f643ac5b07006f44f
dhoko <aurelien@procheo.fr>
2013-07-14 23:35:03 +0200

    Update

--- app/js/ListController.js
+++ app/js/ListController.js
@@ -3,10 +3,16 @@
 TSManager.controller('ListCtrl', function ($scope, Tasks) {
 
 	$scope.todos =  Tasks.query();
+	$scope.alerts = [];
 
+	$scope.alert = function() {
+	}
 	$scope.submitForm = function (data) {
-
 		Tasks.update({id: data.id},data);
-		console.log(data);
+		$scope.alerts.push({
+		    "type": "success",
+		    "title": "Update success for " + data.name,
+		    "content": "Isn't it wonderful ?"
+		});
 	}
 });
