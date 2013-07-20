4c201fa51bf6d9544583fea83f52aff1f3ca6e96
dhoko <aurelien@procheo.fr>
2013-07-20 18:20:02 +0200

    Mise en forme finale

--- app/js/ListController.js
+++ app/js/ListController.js
@@ -3,6 +3,8 @@
 TSManager.controller('ListCtrl', function ($scope, Tasks) {
 
 	$scope.todos =  Tasks.query();
+	$scope.from = '-done';
+	$scope.reverse = true;
 	$scope.alerts = [];
 	$scope.modal = {
 	  "content": "Hello Modal",
@@ -12,6 +14,14 @@ TSManager.controller('ListCtrl', function ($scope, Tasks) {
 	$scope.create = function (task) {
 		Tasks.save(task);
 		$scope.todos.push(task);
+	}
+
+	$scope.remove = function (task) {
+
+		var position = $scope.todos.indexOf(task);
+		$scope.todos.splice(position,1);
+		// console.log($scope.todos.indexOf(task) + ' - Position de la tach courant');
+		Tasks.remove({id: task.id});
 	}
 
 	$scope.alert = function() {
